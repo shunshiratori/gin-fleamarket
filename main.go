1a42b56cb4e2e8defad06ca2ec6f08d1c6e49500
shun <shun.soccer.shun@gmail.com>
2025-12-18 21:22:31 +0900

    feat: 商品検索機能

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	contorollers "gin-fleamarket/controllers"
+	"gin-fleamarket/models"
+	repositoties "gin-fleamarket/repositories"
+	"gin-fleamarket/services"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	items := []models.Item{
+		{ID: 1, Name: "商品1", Price: 1000, Description: "説明1", SoldOut: false},
+		{ID: 2, Name: "商品2", Price: 2000, Description: "説明2", SoldOut: false},
+		{ID: 3, Name: "商品3", Price: 3000, Description: "説明3", SoldOut: true},
+	}
+
+	itemMemoryRepository := repositoties.NewItemMemoryRepository(items)
+	itemService := services.NewItemService(itemMemoryRepository)
+	itemControlloer := contorollers.NewItemController(itemService)
+
+	router := gin.Default()
+	router.GET("/items", itemControlloer.FindAll)
+	router.GET("/items/:id", itemControlloer.FindById)
+	router.Run() // デフォルトで0.0.0.0:8080で待機します
+}
